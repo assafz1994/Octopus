7d7d5f531a2fb9f4deb6f809332b8d7fab0affa9
Ohad1 <37572368+Ohad1@users.noreply.github.com>
2020-12-10 22:58:43 +0200

    add Visitor

---
+++ OctopusCore/Parser/Parser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Antlr4.Runtime;
+
+namespace OctopusCore.Parser
+{
+    class Parser : IParser
+    {
+        public async Task<QueryInfo> ParseQuery(string query)
+        {
+            var target = new AntlrInputStream(query);
+            var lexer = new OctopusQLLexer(target);
+            var tokens = new CommonTokenStream(lexer);
+            var parser = new OctopusQLParser(tokens)
+            {
+                BuildParseTree = true
+            };
+
+            var tree = parser.r();
+            var queryExtractor = new QueryExtractor();
+            var output = queryExtractor.Visit(tree);
+            return output;
+        }
+    }
+}
---
+++ OctopusCore/Parser/QueryExtractor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Antlr4.Runtime.Misc;
+
+namespace OctopusCore.Parser
+{
+    class QueryExtractor : OctopusQLBaseVisitor<QueryInfo>
+    {
+        public override QueryInfo VisitR([NotNull] OctopusQLParser.RContext context)
+        {
+            Console.WriteLine();
+            return base.VisitR(context);
+        }
+    }
+}
