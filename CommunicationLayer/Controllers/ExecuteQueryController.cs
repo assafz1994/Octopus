8921703bb12b4e4a068cf8e907342906755a3148
NoaFeiger <noafeiger29@gmail.com>
2020-12-12 16:23:53 +0200

    Add Communication Layer: Server side

---
+++ CommunicationLayer/Controllers/ExecuteQueryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CommunicationLayer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExecuteQueryController : ControllerBase
+    { 
+        private readonly ILogger<ExecuteQueryController> _logger;
+        //private readonly OctopusService _octopusService;
+        public ExecuteQueryController(ILogger<ExecuteQueryController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public void Get()
+        {
+        }
+
+        [HttpGet]
+        [Route("ExecuteQuery")]
+        //Example: https://localhost:44398/executequery/ExecuteQuery?query=noa
+        public string ExecuteQuery(string query)
+        {
+           // Task resQuery = _octopusService.ExecuteQuery(query);
+            return query;
+        }
+    }
+}
