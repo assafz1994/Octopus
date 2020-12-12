31b17c47ee1cb81f5acef17694c9cb253e45a5cc
Ohad1 <37572368+Ohad1@users.noreply.github.com>
2020-12-12 17:32:17 +0200

    Revert "Revert "Finish SelectQueryInfo""
    This reverts commit 3ca7dcec41a157a47adf1469c46e58a109acf264.

---
+++ Tests/Parser/TestSelectQueryInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using NUnit.Framework;
+using OctopusCore.Parser;
+
+namespace Tests.Parser
+{
+    [TestFixture]
+    public class TestSelectQueryInfo
+    {
+        private IParser _parser;
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _parser = new OctopusCore.Parser.Parser();
+        }
+        public static IEnumerable<TestCaseData> TestValidSelectQueryInfoParameters
+        {
+            get
+            {
+                yield return new TestCaseData(EligibleTest1, new List<int> { 1, 1, 1, 1 }).
+                    SetName("Eligible OrComposite Discount - True Pred");
+                yield return new TestCaseData(EligibleTest2, new List<int> { 5, 1, 1, 1 }).
+                    SetName("Eligible OrComposite Discount - Min Quantity with one true");
+                yield return new TestCaseData(EligibleTest3, new List<int> { 5, 10, 15, 20 }).
+                    SetName("Eligible OrComposite Discount - multiple products 1");
+                yield return new TestCaseData(EligibleTest4, new List<int> { 5, 10, 15, 20 }).
+                    SetName("Eligible OrComposite Discount - multiple products 2");
+                yield return new TestCaseData(EligibleTest5, new List<int> { 5, 10, 15, 20 }).
+                    SetName("Eligible OrComposite Discount - multiple products 3");
+                yield return new TestCaseData(EligibleTest6, new List<int> { 5, 10, 15, 20 }).
+                    SetName("Eligible OrComposite Discount - multiple products 4");
+            }
+        }
+        [Test, TestCaseSource(nameof(TestValidSelectQueryInfoParameters))]
+        public void TestValidSelectQueryInfo(string query, SelectQueryInfo selectQueryInfo)
+        {
+            Assert.AreEqual(selectQueryInfo, );
+        }
+    }
+}
