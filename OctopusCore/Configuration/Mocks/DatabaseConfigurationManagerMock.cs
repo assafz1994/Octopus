b61a98f7512144434668720c34c4c729d672bf6c
assafz1994 <assafz1994@gmail.com>
2020-12-15 20:41:44 +0200

    Add simple test to analyzer (#10)

---
+++ OctopusCore/Configuration/Mocks/DatabaseConfigurationManagerMock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using OctopusCore.DbHandlers;
+
+namespace OctopusCore.Configuration.Mocks
+{
+    public class DatabaseConfigurationManagerMock : IDatabaseConfigurationManager
+    {
+        private readonly Dictionary<string, Dictionary<string, string>> _entityTypeToFieldNameToDataBaseKeyMappings;
+
+        public DatabaseConfigurationManagerMock(Dictionary<string, Dictionary<string, string>> entityTypeToFieldNameToDataBaseKeyMappings)
+        {
+            _entityTypeToFieldNameToDataBaseKeyMappings = entityTypeToFieldNameToDataBaseKeyMappings;
+        }
+
+        public string GetFieldDatabaseKey(string entityType, string fieldName)
+        {
+            return _entityTypeToFieldNameToDataBaseKeyMappings[entityType][fieldName];
+        }
+
+        public IDbHandler ResolveDatabaseHandler(string databaseKey)
+        {
+            return null;
+        }
+    }
+}
---
+++ Tests/Analyzer/AnalyzerTests.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Autofac;
+using NUnit.Framework;
+using OctopusCore.Analyzer;
+using OctopusCore.Analyzer.Jobs;
+using OctopusCore.Configuration;
+using OctopusCore.Configuration.Mocks;
+using OctopusCore.Parser;
+
+namespace Tests
+{
+    [TestFixture]
+    public class AnalyzerTests
+    {
+        private const string EntityName = "Entity1";
+        private const string IdField = "Id";
+        private const string NameField = "Name";
+        private const string DatabaseKey = "sql1";
+
+        private static IContainer _container;
+        private static SelectQueryInfo _selectQueryInfo;
+
+
+        [OneTimeSetUp]
+        public static void SetUp()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<Analyzer>().As<IAnalyzer>();
+
+            var databaseConfigurationManagerMock = CreateDatabaseConfigurationManagerMock();
+            builder.RegisterInstance(databaseConfigurationManagerMock).As<IDatabaseConfigurationManager>();
+
+            _container = builder.Build();
+
+            _selectQueryInfo = new SelectQueryInfo
+            {
+                Entity = EntityName,
+                Fields = new List<string>
+                {
+                    IdField,
+                    NameField
+                }
+            };
+
+            IDatabaseConfigurationManager CreateDatabaseConfigurationManagerMock()
+            {
+                var entityTypeToFieldNameToDatabaseKeyMappings = new Dictionary<string, Dictionary<string, string>>
+                {
+                    {
+                        EntityName, new Dictionary<string, string>
+                        {
+                            {IdField, DatabaseKey},
+                            {NameField, DatabaseKey}
+                        }
+                    }
+                };
+
+                return new DatabaseConfigurationManagerMock(entityTypeToFieldNameToDatabaseKeyMappings);
+            }
+        }
+
+
+        [Test]
+        public void TestAnalyzeSelectQuery()
+        {
+            using var scope = _container.BeginLifetimeScope();
+            var analyzer = scope.Resolve<IAnalyzer>();
+            var actualWorkPlan = analyzer.AnalyzeQuery(_selectQueryInfo);
+
+            Assert.AreEqual(1, actualWorkPlan.Jobs.Count);
+            var job = actualWorkPlan.Jobs.Single();
+            Assert.IsInstanceOf<QueryJob>(job);
+            var queryJob = (QueryJob) job;
+            Assert.AreEqual(EntityName, queryJob.EntityType);
+            Assert.That(queryJob.FieldsToSelect, Is.EquivalentTo(new[]
+            {
+                IdField,
+                NameField
+            }));
+        }
+    }
+}
