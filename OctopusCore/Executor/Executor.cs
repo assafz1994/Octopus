cd61199d5119314023118c3f768739fc1c17c0bc
assafz1994 <assafz1994@gmail.com>
2020-12-15 21:19:21 +0200

    implement executor (#11)

---
+++ OctopusCore/Executor/Executor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using OctopusCore.Contract;
+
+namespace OctopusCore.Executor
+{
+    public class Executor:IExecutor
+    {
+        public Task<ExecutionResult> ExecuteWorkPlanAsync(WorkPlan workPlan)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
