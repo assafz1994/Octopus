827f5edb0f87ba9b2efdd97f47e50561e37bfcc0
NoaFeiger <37437989+NoaFeiger@users.noreply.github.com>
2020-12-15 20:11:17 +0200

    Noa/Communication Layer (#1)
    * Add Communication Layer:
    Server side
    
    * small fixes
    
    * delete obj and bin

---
+++ CommunicationLayer/Controllers/ExecuteQueryController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CommunicationLayer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExecuteQueryController : ControllerBase
+    { 
+        //private readonly OctopusService _octopusService;
+        public ExecuteQueryController()
+        {
+        }
+
+        [HttpGet]
+        public void Get()
+        {
+        }
+
+        [HttpGet]
+        [Route("ExecuteQuery")]
+        //Example: https://localhost:44398/executequery/ExecuteQuery?query=blabla
+        public string ExecuteQuery(string query)
+        {
+           // Task resQuery = _octopusService.ExecuteQuery(query);
+            return query;
+        }
+    }
+}
