edf4ad135582e05f4101e9043f59f00b54781bd1
buzagly <37505183+buzagly@users.noreply.github.com>
2020-12-16 17:59:31 +0200

    add sqliteHandler (#13)
    * add sqliteHandler
    
    * convert expression to sql expected expression
    
    * fix table name
    
    Co-authored-by: ydyb9 <ydyb9@DESKTOP-3LRTMJG>
    Co-authored-by: assafz1994 <assafz1994@gmail.com>

--- OctopusCore/Contract/ExecutionResult.cs
+++ OctopusCore/Contract/ExecutionResult.cs
@@ -1,6 +1,16 @@
 ﻿namespace OctopusCore.Contract
 {
-    public class ExecutionResult
-    {
+    public class ExecutionResult{
+
+        public List<Dictionary<string, object>> ListOfFieldsToValueMap { get; set; }
+        public ExecutionResult(List<Dictionary<string,object>> listOfFieldsToValueMap)
+        {
+            ListOfFieldsToValueMap = listOfFieldsToValueMap;
+        }
+
+        public ExecutionResult()
+        {
+            
+        }
     }
 }
---
+++ OctopusCore/DbHandlers/SqliteDbHandler.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using OctopusCore.Contract;
+using OctopusCore.Parser;
+using OctopusCore.Parser.Filters;
+
+namespace OctopusCore.DbHandlers
+{
+    public class SqliteDbHandler : IDbHandler
+    {
+        public Task<ExecutionResult> ExecuteQueryWithFiltersAsync(IReadOnlyCollection<string> fieldsToSelect,
+            IReadOnlyCollection<Filter> filters, string entityType)
+        {
+            using var connection = new SqliteConnection(GetConnectionString());
+            connection.Open();
+
+            var table = GetTable(entityType);
+            var fields = string.Join(",", fieldsToSelect);
+            var conditions = ConvertFiltersToWhereStatement(filters);
+
+            var command = connection.CreateCommand();
+            //todo prevent sql injection
+            command.CommandText = "SELECT " + fields + " FROM " + table + " WHERE " + conditions;
+
+            var result = ExecuteCommand(fieldsToSelect, command);
+
+            return Task.FromResult(new ExecutionResult(result));
+        }
+
+        private string GetTable(string entityType)
+        {
+            //assumption: entity is one(e.g. 'User'), table will be many(e.g. 'Users')
+            return entityType + 's';
+        }
+
+        private string GetFilterOperator(Filter filter)
+        {
+            if (filter is EqFilter) return "=";
+
+            throw new ArgumentException("Filter type is not supported");
+        }
+
+        private string GetConnectionString()
+        {
+            //todo should receive this from Configuration
+            return "Data Source=DataBases\\demoDb.db";
+        }
+
+        private static List<Dictionary<string, object>> ExecuteCommand(IReadOnlyCollection<string> fieldsToSelect,
+            SqliteCommand command)
+        {
+            using var reader = command.ExecuteReader();
+            var output = new List<Dictionary<string, object>>();
+
+            while (reader.Read())
+            {
+                var fieldToValueMap = fieldsToSelect.ToDictionary(field => field, field => reader[field]);
+
+                output.Add(fieldToValueMap);
+            }
+
+            return output;
+        }
+
+        //when expression represents field of type string, it expect it to be surround by ' '.
+        //if field is type int, this is not needed, but still works both way, so for simplicity we do this conversion for every expression
+        private string ConvertExpression(string expression)
+        {
+            return "\'" + expression + "\'";
+        }
+
+        private string ConvertFiltersToWhereStatement(IReadOnlyCollection<Filter> filters)
+        {
+            if (filters.Count == 0)
+            {
+                //todo check why 'true' not working. think about alternative
+                return "1=1";
+            }
+
+            var filtersAsString = new List<string>();
+            foreach (var filter in filters)
+            {
+                if (filter.FieldNames.Count > 1) throw new ArgumentException("Fields with Include are not supported");
+
+                var filterAsString = new StringBuilder().Append(filter.FieldNames[0]).Append(GetFilterOperator(filter))
+                    .Append(ConvertExpression(filter.Expression)).ToString();
+                filtersAsString.Add(filterAsString);
+            }
+
+            var conditions = string.Join(" and ", filtersAsString);
+            return conditions;
+        }
+    }
+}
