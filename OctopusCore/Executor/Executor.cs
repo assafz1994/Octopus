ed5fe3866aa344c1f1ab5b0d674c066ca2fe138d
Ohad1 <37572368+Ohad1@users.noreply.github.com>
2020-12-24 22:18:19 +0200

    add subQuery

--- OctopusCore/Executor/Executor.cs
+++ OctopusCore/Executor/Executor.cs
@@ -8,6 +8,8 @@ namespace OctopusCore.Executor
     {
         public async Task<ExecutionResult> ExecuteWorkPlanAsync(WorkPlan workPlan)
         {
+            foreach (var job in workPlan.SubQueryWorkPlans.Values.SelectMany(x => x.Jobs)) await job.ExecuteAsync();
+            
             foreach (var job in workPlan.Jobs) await job.ExecuteAsync();
 
             return workPlan.Jobs.Last().Result as ExecutionResult;
