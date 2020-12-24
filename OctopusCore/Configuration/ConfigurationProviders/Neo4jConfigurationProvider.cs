69c0b69696d8c59208f5c251a08e3535d181303e
ydyb9 <ydyb9@DESKTOP-3LRTMJG>
2020-12-24 19:00:20 +0200

    add skeletons

---
+++ OctopusCore/Configuration/ConfigurationProviders/Neo4jConfigurationProvider.cs
@@ -0,0 +1,12 @@
+﻿namespace OctopusCore.Configuration.ConfigurationProviders
+{
+    public class Neo4jConfigurationProvider
+    {
+        public string ConnectionString { get; }
+
+        public Neo4jConfigurationProvider(Scheme scheme, DbConfiguration dbConfiguration)
+        {
+            ConnectionString = dbConfiguration.ConnectionString;
+        }
+    }
+}
---
+++ OctopusCore/DbHandlers/Neo4jDbHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using OctopusCore.Configuration.ConfigurationProviders;
+using OctopusCore.Contract;
+using OctopusCore.Parser;
+
+namespace OctopusCore.DbHandlers
+{
+    public class Neo4jDbHandler: IDbHandler
+    {
+        private readonly Neo4jConfigurationProvider _configurationProvider;
+
+        public Neo4jDbHandler(Neo4jConfigurationProvider configurationProvider)
+        {
+            _configurationProvider = configurationProvider;
+        }
+        public Task<ExecutionResult> ExecuteQueryWithFiltersAsync(IReadOnlyCollection<string> fieldsToSelect, IReadOnlyCollection<Filter> filters, string entityType)
+        {
+            return null;
+        }
+    }
+}
