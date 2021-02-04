088c69a82f56900a69f1cb51007fda3a25b45aee
Ohad1 <37572368+Ohad1@users.noreply.github.com>
2021-02-04 19:34:57 +0200

    init

---
+++ OctopusCore/DbHandlers/CassandraDbHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using OctopusCore.Contract;
+using OctopusCore.Parser;
+
+namespace OctopusCore.DbHandlers
+{
+    public class CassandraDbHandler : IDbHandler
+    {
+        public Task<ExecutionResult> ExecuteQueryWithFiltersAsync(IReadOnlyCollection<string> fieldsToSelect, IReadOnlyCollection<Filter> filters, string entityType)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
