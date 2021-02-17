e885a14e030852ca7a622f03d1904a2a3eaa7ed2
Ohad1 <37572368+Ohad1@users.noreply.github.com>
2021-02-17 10:46:08 +0200

    update scheme

--- OctopusCore/Configuration/ConfigurationProviders/CassandraConfigurationProvider.cs
+++ OctopusCore/Configuration/ConfigurationProviders/CassandraConfigurationProvider.cs
@@ -8,14 +8,18 @@ namespace OctopusCore.Configuration.ConfigurationProviders
     {
         public string ConnectionString { get; }
         public string KeySpace { get; }
+        public List<Entity> Entities { get; set; }
         public CassandraConfigurationProvider(Scheme scheme, DbConfiguration dbConfiguration)
         {
             ConnectionString = dbConfiguration.ConnectionString;
             KeySpace = dbConfiguration.KeySpace;
+            Entities = dbConfiguration.Entities;
         }
 
         public string GetTableName(string entityType, IReadOnlyCollection<Filter> filters)
         {
+            if (filters.Count == 0) return $"{entityType}_table";
+            var tableByField = Entities.First(x => x.Name.Equals(entityType)).Fields;
             return filters.Count > 0 
                 ? $"{entityType}_by_{filters.First().FieldNames.First()}_table" 
                 : $"{entityType}_table";
