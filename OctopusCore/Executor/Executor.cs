28312744296ebd6d9219b63189cb7c4338bcba75
Assaf Z <assafz1994@gmail.com>
2021-02-27 14:50:17 +0200

    Demo Fixes

--- OctopusCore/Executor/Executor.cs
+++ OctopusCore/Executor/Executor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using OctopusCore.Contract;
@@ -8,6 +9,11 @@ namespace OctopusCore.Executor
     {
         public async Task<ExecutionResult> ExecuteWorkPlanAsync(WorkPlan workPlan)
         {
+            if (workPlan.Jobs == null || workPlan.Jobs.Any() == false)
+            {
+                throw new Exception($"{nameof(WorkPlan)} Can not be null or empty");
+            }
+
             foreach (var job in workPlan.Jobs) await job.ExecuteAsync();
 
             return workPlan.Jobs.Last().Result as ExecutionResult;
--- OctopusDemo/Program.cs
+++ OctopusDemo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Dynamic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,16 +12,48 @@ namespace OctopusDemo
     {
         static async Task Main(string[] args)
         {
-            var query = @"from Person p | where p.name == ""Assaf"" | where p.age == 26 |select p(name,age)";
-
-            using (var client = new OctopusClient("http://localhost:5000"))
+            const string defaultEndpoint = "http://localhost:5000";
+            Console.WriteLine($"Enter the Octopus Server Endpoint(Default is {defaultEndpoint}. leave empty to use the defalut value):");
+            var endpoint = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(endpoint))
             {
-                var entities = await client.ExecuteQuery(query);
-                foreach (var entity in entities)
+                endpoint = defaultEndpoint;
+            }
+            var input = "";
+            while (input?.Equals("exit",StringComparison.InvariantCultureIgnoreCase) == false)
+            {
+                try
                 {
-                    Console.WriteLine(entity.name);
+                    Console.WriteLine("Enter Query or Write Exit:");
+                    input = Console.ReadLine();
+                   // input = @"from Person p | where p.name == ""Assaf"" | where p.age == 26 |select p(name,age)";
+                    using (var client = new OctopusClient(endpoint))
+                    {
+                        var entities = await client.ExecuteQuery(input);
+                        int entityCount = 1;
+
+                        foreach (var entity in entities)
+                        {
+                            Console.WriteLine($"Entity {entityCount}:");
+                            foreach (KeyValuePair<string,object> kvp in entity)
+                            {
+                                string name = kvp.Key;
+                                object value = kvp.Value;
+                                Console.WriteLine("{0}={1}", name, value);
+                            }
+
+                            entityCount++;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    input = "";
+                    Console.WriteLine("Error:\n\n");
+                    Console.WriteLine(e);
                 }
             }
+
         }
     }
 }
