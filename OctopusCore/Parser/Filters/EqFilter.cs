c0a47de8af46472a4faabff58d1c73c8912638a8
Ohad1 <37572368+Ohad1@users.noreply.github.com>
2021-03-19 18:37:27 +0200

    eqfilter fix

--- OctopusCore/Parser/Filters/EqFilter.cs
+++ OctopusCore/Parser/Filters/EqFilter.cs
@@ -28,6 +28,7 @@ namespace OctopusCore.Parser.Filters
         {
             if (obj == null) return false;
             if (obj == this) return true;
+            if (!(obj is EqFilter)) return false;
             var eqFilter = (EqFilter) obj;
             return FieldNames.SequenceEqual(eqFilter.FieldNames)
                    && Expression.Equals(eqFilter.Expression)
