152d08e3823ac4a834c2bae927a1342ac40dfb13
Ohad1 <37572368+Ohad1@users.noreply.github.com>
2021-03-23 09:09:44 +0200

    classes

---
+++ OctopusCore/Parser/InsertQueryInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace OctopusCore.Parser
+{
+    public class InsertQueryInfo : QueryInfo
+    {
+        public List<ParserEntity> ParserEntities { get; set; }
+        public List<string> EntityReps { get; set; }
+        
+
+        public InsertQueryInfo(List<ParserEntity> parserEntities, List<string> entityReps)
+        {
+            SubQueries = new Dictionary<string, QueryInfo>();
+            ParserEntities = parserEntities;
+            EntityReps = entityReps;
+        }
+    }
+}
---
+++ OctopusCore/Parser/ParserEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OctopusCore.Parser
+{
+    public class ParserEntity
+    {
+        public string EntityType;
+        public Dictionary<string, dynamic> Fields { get; set; }
+
+        public ParserEntity(string entityType, Dictionary<string, dynamic> fields)
+        {
+            EntityType = entityType;
+            Fields = fields;
+        }
+    }
+}
