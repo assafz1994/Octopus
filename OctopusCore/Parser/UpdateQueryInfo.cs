442bbfd51cf41995c0ed7084053f4019f86f76ff
Ohad1 <37572368+Ohad1@users.noreply.github.com>
2021-04-18 18:54:29 +0300

    start

---
+++ OctopusCore/Parser/UpdateQueryInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OctopusCore.Parser
+{
+    class UpdateQueryInfo : QueryInfo
+    {
+        public string Entity { get; set; }
+        public List<ParserEntity> ParserEntities { get; set; }
+        public UpdateQueryInfo()
+        {
+            SubQueries = new Dictionary<string, QueryInfo>();
+            ParserEntities = new List<ParserEntity>();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (obj == this) return true;
+            if (!(obj is UpdateQueryInfo updateQueryInfo)) return false;
+            return SubQueries.Values.SequenceEqual(updateQueryInfo.SubQueries.Values);
+
+        }
+    }
+}
