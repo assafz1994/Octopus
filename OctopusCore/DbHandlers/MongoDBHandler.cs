e4ae9e2bc0cf1f7b2a90606ba1331cc6655139bc
NoaFeiger <37437989+NoaFeiger@users.noreply.github.com>
2021-04-19 18:21:07 +0300

    Add support of delete query in mongodb (#42)
    * Add support of delete query in mongodb
    
    * Changed mongodb database to a new one in the configuration file

--- OctopusCore/DbHandlers/MongoDBHandler.cs
+++ OctopusCore/DbHandlers/MongoDBHandler.cs
@@ -1,12 +1,14 @@
-﻿using OctopusCore.Contract;
-using OctopusCore.Parser;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
+
+using OctopusCore.Contract;
+using OctopusCore.Parser;
+using OctopusCore.Parser.Filters;
 using OctopusCore.Configuration.ConfigurationProviders;
 using MongoDB.Driver;
 using MongoDB.Bson;
-using OctopusCore.Parser.Filters;
 
 namespace OctopusCore.DbHandlers
 {
@@ -29,17 +31,24 @@ namespace OctopusCore.DbHandlers
             var collection = db.GetCollection<BsonDocument>(collectionName);
             
             var fieldsToSelectWithGuid = new List<string>(fieldsToSelect);
-            fieldsToSelectWithGuid.Add("guid");
+            var guidIsRequested = false;
 
+            if (!fieldsToSelect.Contains("guid"))
+            {
+                fieldsToSelectWithGuid.Add("guid");
+            }
+            else
+            {
+                guidIsRequested = true;
+            }
             ProjectionDefinition<BsonDocument> project = BuildProjection(fieldsToSelectWithGuid);
 
             FilterDefinition<BsonDocument> conditions = BuildFilters(filters);
            
-            var result = ExecuteCommand(collection, project, conditions);
+            var result = ExecuteCommand(collection, project, conditions, guidIsRequested);
 
             return Task.FromResult(new ExecutionResult(entityType, result));
         }
-
 
         public Task<ExecutionResult> ExecuteInsertQuery(string entityType, IReadOnlyDictionary<string, dynamic> fields)
         {
@@ -52,7 +61,8 @@ namespace OctopusCore.DbHandlers
             BsonDocument entityToInsert = new BsonDocument();
             foreach (var key in fields.Keys)
             {
-                entityToInsert.Add(key, fields[key]);
+                var val = fields[key] is string ? GetExpression(fields[key]) : fields[key];
+                entityToInsert.Add(key, val);
             }
             
             collection.InsertOne(entityToInsert);
@@ -62,17 +72,35 @@ namespace OctopusCore.DbHandlers
 
         public Task<ExecutionResult> ExecuteDeleteQuery(string entityType, IReadOnlyCollection<string> guidCollection)
         {
-            throw new NotImplementedException();
+            var guidsToDelete = new List<Guid>();
+            foreach (var guid in guidCollection)
+            {
+                guidsToDelete.Add(Guid.Parse(guid));
+            }
+
+            MongoClient dbClient = new MongoClient(); // no need to insert connection string -> local db is the default
+            var databaseName = MongoUrl.Create(_configurationProvider.ConnectionString).DatabaseName;
+            var db = dbClient.GetDatabase(databaseName);
+            string collectionName = _configurationProvider.GetTableName(entityType);
+            var collection = db.GetCollection<BsonDocument>(collectionName);
+           
+            var deleteFilter = Builders<BsonDocument>.Filter.In("guid", guidsToDelete);
+            collection.DeleteMany(deleteFilter);
+           
+            return Task.FromResult(new ExecutionResult(entityType, new Dictionary<string, EntityResult>()));
         }
 
-        private Dictionary<string, EntityResult> ExecuteCommand(IMongoCollection<BsonDocument> collection, ProjectionDefinition<BsonDocument> project, FilterDefinition<BsonDocument> conditions)
+        private Dictionary<string, EntityResult> ExecuteCommand(IMongoCollection<BsonDocument> collection, ProjectionDefinition<BsonDocument> project, FilterDefinition<BsonDocument> conditions, bool guidIsRequested)
         {
             List<BsonDocument> result = (conditions == null) ? collection.Find(_ => true).Project(project).ToList() :  collection.Find(conditions).Project(project).ToList();
             Dictionary<string, EntityResult> entityResults = new Dictionary<string, EntityResult>();
             foreach (var entity in result)
             {
                 var entityWithOutGuid = entity.ToDictionary();
-                entityWithOutGuid.Remove("guid");
+                if (!guidIsRequested)
+                {
+                    entityWithOutGuid.Remove("guid");
+                }
                 entityResults.Add(entity["guid"].ToString(), new EntityResult(entityWithOutGuid));
             }
 
@@ -117,23 +145,25 @@ namespace OctopusCore.DbHandlers
             {
                 if (filter.FieldNames.Count > 1)
                     throw new ArgumentException("Fields with Include are not supported");
-                
+
                 FilterDefinition<BsonDocument> filterOp;
+                dynamic exp = filter.Expression is string ? GetExpression(filter.Expression) : filter.Expression;
+                
                 switch (GetFilterOperator(filter))
                 {
                     case "=":
                         {
-                            filterOp = builder.Eq(filter.FieldNames[0], filter.Expression);
+                            filterOp = builder.Eq(filter.FieldNames[0], exp);
                             break;
                         }
                     case ">":
                         {
-                            filterOp = builder.Gt(filter.FieldNames[0], filter.Expression);
+                            filterOp = builder.Gt(filter.FieldNames[0], exp);
                             break;
                         }
                     case "<":
                         {
-                            filterOp = builder.Lt(filter.FieldNames[0], filter.Expression);
+                            filterOp = builder.Lt(filter.FieldNames[0], exp);
                             break;
                         }
 
@@ -146,6 +176,13 @@ namespace OctopusCore.DbHandlers
             }
 
             return filterOutput;
+        }
+
+        private static string GetExpression(string expression)
+        {
+            if (!expression.StartsWith("\"") || !expression.EndsWith("\"")) return expression;
+            var res = expression.Trim('"');
+            return $"{res}";
         }
     }
 }
