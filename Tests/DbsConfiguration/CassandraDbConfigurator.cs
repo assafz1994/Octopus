64e502c2fd15dc231cb72a91dca8f3a16bba3075
Ohad1 <37572368+Ohad1@users.noreply.github.com>
2021-04-30 15:41:04 +0300

    configurator

--- Tests/DbsConfiguration/CassandraDbConfigurator.cs
+++ Tests/DbsConfiguration/CassandraDbConfigurator.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Cassandra;
 
 namespace Tests.DbsConfiguration
 {
     class CassandraDbConfigurator
     {
+        private Cluster _cluster;
+        private ISession _session;
+        private string _connectionString = "127.0.0.1";
+        private string _keySpace = "OctopusTests";
+        private string _cassandraCreateTableFile = "CreateTableCassandra.txt";
+
+        public CassandraDbConfigurator()
+        {
+            _cluster = Cluster.Builder()
+                .AddContactPoint(_connectionString)
+                .Build();
+            _session = _cluster.Connect(_keySpace);
+        }
+
+        public void SetUpDb()
+        {
+            _session.Execute(System.IO.File.ReadAllText(_cassandraCreateTableFile));
+        }
     }
 }
--- Tests/DbsConfiguration/SqliteDbConfigurator.cs
+++ Tests/DbsConfiguration/SqliteDbConfigurator.cs
@@ -1,16 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Data.Sqlite;
 
 namespace Tests.DbsConfiguration
 {
     class SqliteDbConfigurator
     {
         // private string _initFilePath = "somepath";
-        private string _initQuery = "";
+        private string _sql1ConnectionString = "Data Source=DataBases\\sqlite1_test_db.db";
+        private string _sql2ConnectionString = "Data Source=DataBases\\sqlite2_test_db.db";
+        private string _sql1CreateTableFile = "CreateTableSqlite1.txt";
+        private string _sql2CreateTableFile = "CreateTableSqlite2.txt";
         public void SetUpDb()
         {
+            CreateTables(_sql1ConnectionString, _sql1CreateTableFile);
+            CreateTables(_sql2ConnectionString, _sql2CreateTableFile);
+        }
 
+        private void CreateTables(string connectionString, string createTableFile)
+        {
+            using var connection = new SqliteConnection(connectionString);
+            var command = connection.CreateCommand();
+            command.CommandText = System.IO.File.ReadAllText(createTableFile);
+            command.ExecuteNonQueryAsync();
         }
     }
 }
