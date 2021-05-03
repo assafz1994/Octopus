ce7e9f5cc2232ab748e0f9f491fe6d71f82891d9
Ohad1 <37572368+Ohad1@users.noreply.github.com>
2021-05-03 21:47:10 +0300

    tests cases

--- OctopusCore/DbHandlers/CassandraDbHandler.cs
+++ OctopusCore/DbHandlers/CassandraDbHandler.cs
@@ -16,50 +16,45 @@ namespace OctopusCore.DbHandlers
     public class CassandraDbHandler : IDbHandler
     {
         private readonly CassandraConfigurationProvider _configurationProvider;
+        private Cluster _cluster;
+        private ISession _session;
+
         public CassandraDbHandler(CassandraConfigurationProvider configurationProvider)
         {
             _configurationProvider = configurationProvider;
+            _cluster = Cluster.Builder()
+                .AddContactPoint(_configurationProvider.ConnectionString)
+                .Build();
+            _session = _cluster.Connect(_configurationProvider.KeySpace);
         }
         public Task<ExecutionResult> ExecuteQueryWithFiltersAsync(IReadOnlyCollection<string> fieldsToSelect, IReadOnlyCollection<Filter> filters, string entityType,
             List<(string entityType, Field field, List<string> fieldsToSelect)> joinsTuples)
         {
-            var cluster = Cluster.Builder()
-                .AddContactPoint(_configurationProvider.ConnectionString)
-                .Build();
             var fieldsToSelectWithGuid = fieldsToSelect.ToList().Append(StringConstants.Guid).ToList();
             var fields = (fieldsToSelectWithGuid.Count == 1) ? 
                 fieldsToSelectWithGuid.First() : 
                 string.Join(",", fieldsToSelectWithGuid);
             // var table = _configurationProvider.GetTableName(entityType, filters);
             var conditions = ConvertFiltersToWhereStatement(filters);
-            var session = cluster.Connect(_configurationProvider.KeySpace);
             var query = _configurationProvider.AssembleSelectQuery(entityType, fields, filters, conditions);
-            var rs = session.Execute(query);
+            var rs = _session.Execute(query);
             var result = ExecuteCommand(fieldsToSelect, rs);
             return Task.FromResult(new ExecutionResult(entityType, result));
         }
 
         public Task<ExecutionResult> ExecuteInsertQuery(string entityType, IReadOnlyDictionary<string, dynamic> fields)
         {
-            var cluster = Cluster.Builder()
-                .AddContactPoint(_configurationProvider.ConnectionString)
-                .Build();
-            var session = cluster.Connect(_configurationProvider.KeySpace);
             var tables = _configurationProvider.GetTableNames(entityType);
             var queries = AssembleInsertQueries(tables, fields);
             foreach (var query in queries)
             {
-                session.Execute(query);
+                _session.Execute(query);
             }
             return Task.FromResult(new ExecutionResult(entityType, new Dictionary<string, EntityResult>()));
         }
 
         public Task<ExecutionResult> ExecuteDeleteQuery(string entityType, IReadOnlyCollection<string> guidCollection)
         {
-            var cluster = Cluster.Builder()
-                .AddContactPoint(_configurationProvider.ConnectionString)
-                .Build();
-            var session = cluster.Connect(_configurationProvider.KeySpace);
             var tableNamesToTables = _configurationProvider.TableNamesToTables(entityType);
             var tableNames = _configurationProvider.GetTableNames(entityType);
             var mainTable = tableNames.First();
@@ -68,7 +63,7 @@ namespace OctopusCore.DbHandlers
             byFields.Add(StringConstants.Guid);
             var guidList = $"({string.Join(",", guidCollection)})";
             var selectQuery = $"SELECT {string.Join(",", byFields)} FROM {mainTable} WHERE GUID IN {guidList}";
-            var rs = session.Execute(selectQuery);
+            var rs = _session.Execute(selectQuery);
             var entities = new List<Dictionary<string, dynamic>>();
             
             foreach (var row in rs)
@@ -85,7 +80,7 @@ namespace OctopusCore.DbHandlers
                 {
                     deleteQuery += $" AND {field}={FieldToString(entity[field])}";
                 }
-                session.Execute(deleteQuery);
+                _session.Execute(deleteQuery);
             }
             return Task.FromResult(new ExecutionResult(entityType, new Dictionary<string, EntityResult>()));
         }
--- Tests/DBHandlers/CassandraDbUnitTest.cs
+++ Tests/DBHandlers/CassandraDbUnitTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Newtonsoft.Json;
 using NUnit.Framework;
@@ -8,6 +9,8 @@ using OctopusCore.Configuration;
 using OctopusCore.Configuration.ConfigurationProviders;
 using OctopusCore.Contract;
 using OctopusCore.DbHandlers;
+using OctopusCore.Parser;
+using OctopusCore.Parser.Filters;
 using Tests.DbsConfiguration;
 
 namespace Tests.DBHandlers
@@ -18,10 +21,15 @@ namespace Tests.DBHandlers
 
         private CassandraDbHandler _cassandraDbHandler;
 
+        private List<string> _tables;
         [OneTimeSetUp]
         public void OneTimeSetUp()
         {
             _cassandraDbConfigurator = new CassandraDbConfigurator();
+            _tables = new List<string>()
+            {
+
+            };
             var configString = @"{
                 ""Id"": ""cassTests"",
                 ""DbType"": ""Cassandra"",
@@ -107,46 +115,168 @@ namespace Tests.DBHandlers
         [Test]
         public void TestSelect1()
         {
-            var executionResult1 = new ExecutionResult(
-                "person",
+            _cassandraDbConfigurator.SetUpTestSelectNamesOfAnimals();
+            var actualExecutionResult = _cassandraDbHandler
+                .ExecuteQueryWithFiltersAsync(new List<string>() { "food", "height" }, 
+                    new List<Filter>(), 
+                    "animal",
+                    new List<(string entityType, Field field, List<string> fieldsToSelect)>()).Result;
+            var expectedExecutionResult = new ExecutionResult(
+                "animal",
                 new Dictionary<string, EntityResult>()
                 {
                     {
-                        "1", new EntityResult(new Dictionary<string, dynamic>()
+                        "9264f435-d1c7-4f1c-8b84-cf4bdb935641", new EntityResult(new Dictionary<string, dynamic>()
                         {
-                            {"name", "ohad"},
-                            {"age", 26}
+                            {"food", "f1"},
+                            {"height", 23}
                         })
                     },
                     {
-                        "2", new EntityResult(new Dictionary<string, dynamic>()
+                        "e8d706f8-92be-429c-89cc-91973fca7a95", new EntityResult(new Dictionary<string, dynamic>()
                         {
-                            {"name", "Noa"},
-                            {"age", 24}
+                            {"food", "f23"},
+                            {"height", 23}
+                        })
+                    },
+                    {
+                        "f443f95a-3d8f-4786-b3e6-0db8b790f7e6", new EntityResult(new Dictionary<string, dynamic>()
+                        {
+                            {"food", "f23"},
+                            {"height", 45}
                         })
                     }
                 });
-            var executionResult2 = new ExecutionResult(
-                "person",
+            
+            Assert.AreEqual(expectedExecutionResult, actualExecutionResult);
+        }
+
+        [Test]
+        public void TestSelect2()
+        {
+            _cassandraDbConfigurator.SetUpTestSelectNamesOfAnimals();
+            var actualExecutionResult = _cassandraDbHandler
+                .ExecuteQueryWithFiltersAsync(new List<string>() { "food", "height" },
+                    new List<Filter>() {new EqFilter(new List<string>() {"height"}, "23")}, 
+                    "animal",
+                    new List<(string entityType, Field field, List<string> fieldsToSelect)>()).Result;
+            var expectedExecutionResult = new ExecutionResult(
+                "animal",
                 new Dictionary<string, EntityResult>()
                 {
                     {
-                        "2", new EntityResult(new Dictionary<string, dynamic>()
+                        "9264f435-d1c7-4f1c-8b84-cf4bdb935641", new EntityResult(new Dictionary<string, dynamic>()
                         {
-                            {"name", "Noa"},
-                            {"age", 24}
+                            {"food", "f1"},
+                            {"height", 23}
                         })
                     },
                     {
-                        "1", new EntityResult(new Dictionary<string, dynamic>()
+                        "e8d706f8-92be-429c-89cc-91973fca7a95", new EntityResult(new Dictionary<string, dynamic>()
                         {
-                            {"name", "ohad"},
-                            {"age", 26}
+                            {"food", "f23"},
+                            {"height", 23}
                         })
                     }
-                    
                 });
-            Assert.AreEqual(executionResult1, executionResult2);
+
+            Assert.AreEqual(expectedExecutionResult, actualExecutionResult);
+        }
+
+        [Test]
+        public void TestSelect3()
+        {
+            _cassandraDbConfigurator.SetUpTestSelectNamesOfAnimals();
+            var actualExecutionResult = _cassandraDbHandler
+                .ExecuteQueryWithFiltersAsync(new List<string>() { "food", "height" },
+                    new List<Filter>()
+                    {
+                        new EqFilter(new List<string>() { "food" }, "\"f23\""),
+                        new EqFilter(new List<string>() { "height" }, "23")
+                    }, 
+                    "animal",
+                    new List<(string entityType, Field field, List<string> fieldsToSelect)>()).Result;
+            var expectedExecutionResult = new ExecutionResult(
+                "animal",
+                new Dictionary<string, EntityResult>()
+                {
+                    {
+                        "e8d706f8-92be-429c-89cc-91973fca7a95", new EntityResult(new Dictionary<string, dynamic>()
+                        {
+                            {"food", "f23"},
+                            {"height", 23}
+                        })
+                    }
+                });
+
+            Assert.AreEqual(expectedExecutionResult, actualExecutionResult);
+        }
+
+        [Test]
+        public void TestSelect4()
+        {
+            _cassandraDbConfigurator.SetUpTestSelectNamesOfAnimals();
+            var actualExecutionResult = _cassandraDbHandler
+                .ExecuteQueryWithFiltersAsync(new List<string>() { "food", "height" },
+                    new List<Filter>(){new EqFilter(new List<string>() { "food" }, "\"f23\""),}, 
+                    "animal",
+                    new List<(string entityType, Field field, List<string> fieldsToSelect)>()).Result;
+            var expectedExecutionResult = new ExecutionResult(
+                "animal",
+                new Dictionary<string, EntityResult>()
+                {
+                    {
+                        "e8d706f8-92be-429c-89cc-91973fca7a95", new EntityResult(new Dictionary<string, dynamic>()
+                        {
+                            {"food", "f23"},
+                            {"height", 23}
+                        })
+                    },
+                    {
+                        "f443f95a-3d8f-4786-b3e6-0db8b790f7e6", new EntityResult(new Dictionary<string, dynamic>()
+                        {
+                            {"food", "f23"},
+                            {"height", 45}
+                        })
+                    }
+                });
+
+            Assert.AreEqual(expectedExecutionResult, actualExecutionResult);
+        }
+
+        [Test]
+        public void TestInsert1()
+        {
+            var actualInsertExecutionResult = _cassandraDbHandler
+                .ExecuteInsertQuery(
+                    "animal", 
+                    new Dictionary<string, dynamic>()
+                    {
+                        {"guid", Guid.NewGuid()},
+                        {"aid", "\"1\""},
+                        {"food", "\"f1\"" },
+                        {"height", 23 }
+                    }).Result;
+            var expectedInsertExecutionResult = new ExecutionResult("animal", new Dictionary<string, EntityResult>());
+
+            Assert.AreEqual(expectedInsertExecutionResult, actualInsertExecutionResult);
+
+            var rowSets = _cassandraDbConfigurator.GetTables()
+                .Select(table => _cassandraDbConfigurator.Execute($"select * from {table}")).ToList();
+            var fieldsToSelect = new List<string>() {"aid", "food", "height"};
+            var expectedEntityResult = new EntityResult(new Dictionary<string, dynamic>()
+            {
+                {"aid", "1"},
+                {"food", "f1" },
+                {"height", 23 }
+            });
+            foreach (var rowSet in rowSets)
+            foreach (var row in rowSet)
+            {
+                var actualEntityResult = new EntityResult(fieldsToSelect.ToDictionary(field => field, field => row.GetValue(typeof(object), field)));
+                Assert.AreEqual(expectedEntityResult, actualEntityResult);
+            }
+            
         }
     }
 }
