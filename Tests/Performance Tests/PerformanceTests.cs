a5ef0756e0e46e3a906140683cb06ebaa89657a6
Ohad1 <37572368+Ohad1@users.noreply.github.com>
2021-06-05 17:05:52 +0300

    tests2

--- Tests/Performance Tests/PerformanceTests.cs
+++ Tests/Performance Tests/PerformanceTests.cs
@@ -13,6 +13,7 @@ namespace Tests.Performance_Tests
         private OctopusClient _client;
         private DbsConfigurator _dbsConfigurator;
         private const int NumOfEntries = 100000;
+        private List<string> _queriesTemplates;
 
         [OneTimeSetUp]
         public void OneTimeSetUp()
@@ -21,6 +22,17 @@ namespace Tests.Performance_Tests
             _dbsConfigurator = new DbsConfigurator();
             _dbsConfigurator.SetUpDbs();
             _dbsConfigurator.InitPerformanceTests();
+            _queriesTemplates = new List<string>()
+            {
+                "from {0} s | select s(name,buyFrom) include(buyFrom(name))",
+                "from {0} s | select s(name,buyFromMany) include(buyFromMany(name))",
+                "from {0} s | select s(name,favoriteSeller) include(favoriteSeller(name))",
+                "from {0} s | select s(name,favoriteSellerMany) include(favoriteSellerMany(name))",
+                "from {0} s | select s(name,sellTo) include(sellTo(name))",
+                "from {0} s | select s(name,sellToMany) include(sellToMany(name))",
+                "from {0} s | select s(name,favoriteBuyer) include(favoriteBuyer(name))",
+                "from {0} s | select s(name,favoriteBuyerMany) include(favoriteBuyerMany(name))"
+            };
         }
 
         [TestCase("buyersqlsql")]
@@ -29,121 +41,157 @@ namespace Tests.Performance_Tests
         [TestCase("buyermongoneo")]
         [TestCase("buyerneoneo")]
         [TestCase("buyermongomongo")]
-        public void TestBuyersBuyFromRelations(string entityName)
+        public void TestPerformance(string entityName)
         {
-            var query = $"from {entityName} s | select s(name,buyFrom) include(buyFrom(name))";
-            var startTime = DateTime.Now;
-            var entities = _client.ExecuteQuery(query).Result;
-            var endTime = DateTime.Now;
-            Assert.AreEqual(entities.Length, NumOfEntries);
+            foreach (var queryTemplate in _queriesTemplates)
+            {
+                var query = string.Format(queryTemplate, entityName);
+                var startTime = DateTime.Now;
+                var entities = _client.ExecuteQuery(query).Result;
+                var endTime = DateTime.Now;
+                var diff = endTime - startTime;
+                System.Diagnostics.Trace.WriteLine(diff);
+                Assert.AreEqual(entities.Length, NumOfEntries);
+            }
         }
 
-        [TestCase("buyersqlsql")]
-        [TestCase("buyersqlmongo")]
-        [TestCase("buyersqlneo")]
-        [TestCase("buyermongoneo")]
-        [TestCase("buyerneoneo")]
-        [TestCase("buyermongomongo")]
-        public void TestBuyersBuyFromManyRelations(string entityName)
-        {
-            var query = $"from {entityName} s | select s(name,buyFromMany) include(buyFromMany(name))";
-            var startTime = DateTime.Now;
-            var entities = _client.ExecuteQuery(query).Result;
-            var endTime = DateTime.Now;
-            var diff = endTime - startTime;
-            System.Diagnostics.Trace.WriteLine(diff);
-            Assert.AreEqual(entities.Length, NumOfEntries);
-        }
+        //This can be used in case you want separation
 
-        [TestCase("buyersqlsql")]
-        [TestCase("buyersqlmongo")]
-        [TestCase("buyersqlneo")]
-        [TestCase("buyermongoneo")]
-        [TestCase("buyerneoneo")]
-        [TestCase("buyermongomongo")]
-        public void TestBuyersFavoriteSellerRelations(string entityName)
-        {
-            var query = $"from {entityName} s | select s(name,favoriteSeller) include(favoriteSeller(name))";
-            var startTime = DateTime.Now;
-            var entities = _client.ExecuteQuery(query).Result;
-            var endTime = DateTime.Now;
-            Assert.AreEqual(entities.Length, NumOfEntries);
-        }
-
-        [TestCase("buyersqlsql")]
-        [TestCase("buyersqlmongo")]
-        [TestCase("buyersqlneo")]
-        [TestCase("buyermongoneo")]
-        [TestCase("buyerneoneo")]
-        [TestCase("buyermongomongo")]
-        public void TestBuyersFavoriteSellerManyRelations(string entityName)
-        {
-            var query = $"from {entityName} s | select s(name,favoriteSellerMany) include(favoriteSellerMany(name))";
-            var startTime = DateTime.Now;
-            var entities = _client.ExecuteQuery(query).Result;
-            var endTime = DateTime.Now;
-            Assert.AreEqual(entities.Length, NumOfEntries);
-        }
-
-
-        [TestCase("sellersqlsql")]
-        [TestCase("sellermongosql")]
-        [TestCase("sellerneosql")]
-        [TestCase("sellerneomongo")]
-        [TestCase("sellerneoneo")]
-        [TestCase("sellermongomongo")]
-        public void TestSellersSellToRelations(string entityName)
-        {
-            var query = $"from {entityName} s | select s(name,sellTo) include(sellTo(name))";
-            var startTime = DateTime.Now;
-            var entities = _client.ExecuteQuery(query).Result;
-            var endTime = DateTime.Now;
-            Assert.AreEqual(entities.Length, NumOfEntries);
-        }
-
-        [TestCase("sellersqlsql")]
-        [TestCase("sellermongosql")]
-        [TestCase("sellerneosql")]
-        [TestCase("sellerneomongo")]
-        [TestCase("sellerneoneo")]
-        [TestCase("sellermongomongo")]
-        public void TestSellersSellToManyRelations(string entityName)
-        {
-            var query = $"from {entityName} s | select s(name,sellToMany) include(sellToMany(name))";
-            var startTime = DateTime.Now;
-            var entities = _client.ExecuteQuery(query).Result;
-            var endTime = DateTime.Now;
-            Assert.AreEqual(entities.Length, NumOfEntries);
-        }
-
-        [TestCase("sellersqlsql")]
-        [TestCase("sellermongosql")]
-        [TestCase("sellerneosql")]
-        [TestCase("sellerneomongo")]
-        [TestCase("sellerneoneo")]
-        [TestCase("sellermongomongo")]
-        public void TestSellersFavoriteBuyerRelations(string entityName)
-        {
-            var query = $"from {entityName} s | select s(name,favoriteBuyer) include(favoriteBuyer(name))";
-            var startTime = DateTime.Now;
-            var entities = _client.ExecuteQuery(query).Result;
-            var endTime = DateTime.Now;
-            Assert.AreEqual(entities.Length, NumOfEntries);
-        }
-
-        [TestCase("sellersqlsql")]
-        [TestCase("sellermongosql")]
-        [TestCase("sellerneosql")]
-        [TestCase("sellerneomongo")]
-        [TestCase("sellerneoneo")]
-        [TestCase("sellermongomongo")]
-        public void TestSellersFavoriteBuyerManyRelations(string entityName)
-        {
-            var query = $"from {entityName} s | select s(name,favoriteBuyerMany) include(favoriteBuyerMany(name))";
-            var startTime = DateTime.Now;
-            var entities = _client.ExecuteQuery(query).Result;
-            var endTime = DateTime.Now;
-            Assert.AreEqual(entities.Length, NumOfEntries);
-        }
+        // [TestCase("buyersqlsql")]
+        // [TestCase("buyersqlmongo")]
+        // [TestCase("buyersqlneo")]
+        // [TestCase("buyermongoneo")]
+        // [TestCase("buyerneoneo")]
+        // [TestCase("buyermongomongo")]
+        // public void TestBuyersBuyFromRelations(string entityName)
+        // {
+        //     var query = $"from {entityName} s | select s(name,buyFrom) include(buyFrom(name))";
+        //     var startTime = DateTime.Now;
+        //     var entities = _client.ExecuteQuery(query).Result;
+        //     var endTime = DateTime.Now;
+        //     var diff = endTime - startTime;
+        //     System.Diagnostics.Trace.WriteLine(diff);
+        //     Assert.AreEqual(entities.Length, NumOfEntries);
+        // }
+        //
+        // [TestCase("buyersqlsql")]
+        // [TestCase("buyersqlmongo")]
+        // [TestCase("buyersqlneo")]
+        // [TestCase("buyermongoneo")]
+        // [TestCase("buyerneoneo")]
+        // [TestCase("buyermongomongo")]
+        // public void TestBuyersBuyFromManyRelations(string entityName)
+        // {
+        //     var query = $"from {entityName} s | select s(name,buyFromMany) include(buyFromMany(name))";
+        //     var startTime = DateTime.Now;
+        //     var entities = _client.ExecuteQuery(query).Result;
+        //     var endTime = DateTime.Now;
+        //     var diff = endTime - startTime;
+        //     System.Diagnostics.Trace.WriteLine(diff);
+        //     Assert.AreEqual(entities.Length, NumOfEntries);
+        // }
+        //
+        // [TestCase("buyersqlsql")]
+        // [TestCase("buyersqlmongo")]
+        // [TestCase("buyersqlneo")]
+        // [TestCase("buyermongoneo")]
+        // [TestCase("buyerneoneo")]
+        // [TestCase("buyermongomongo")]
+        // public void TestBuyersFavoriteSellerRelations(string entityName)
+        // {
+        //     var query = $"from {entityName} s | select s(name,favoriteSeller) include(favoriteSeller(name))";
+        //     var startTime = DateTime.Now;
+        //     var entities = _client.ExecuteQuery(query).Result;
+        //     var endTime = DateTime.Now;
+        //     var diff = endTime - startTime;
+        //     System.Diagnostics.Trace.WriteLine(diff);
+        //     Assert.AreEqual(entities.Length, NumOfEntries);
+        // }
+        //
+        // [TestCase("buyersqlsql")]
+        // [TestCase("buyersqlmongo")]
+        // [TestCase("buyersqlneo")]
+        // [TestCase("buyermongoneo")]
+        // [TestCase("buyerneoneo")]
+        // [TestCase("buyermongomongo")]
+        // public void TestBuyersFavoriteSellerManyRelations(string entityName)
+        // {
+        //     var query = $"from {entityName} s | select s(name,favoriteSellerMany) include(favoriteSellerMany(name))";
+        //     var startTime = DateTime.Now;
+        //     var entities = _client.ExecuteQuery(query).Result;
+        //     var endTime = DateTime.Now;
+        //     var diff = endTime - startTime;
+        //     System.Diagnostics.Trace.WriteLine(diff);
+        //     Assert.AreEqual(entities.Length, NumOfEntries);
+        // }
+        //
+        //
+        // [TestCase("sellersqlsql")]
+        // [TestCase("sellermongosql")]
+        // [TestCase("sellerneosql")]
+        // [TestCase("sellerneomongo")]
+        // [TestCase("sellerneoneo")]
+        // [TestCase("sellermongomongo")]
+        // public void TestSellersSellToRelations(string entityName)
+        // {
+        //     var query = $"from {entityName} s | select s(name,sellTo) include(sellTo(name))";
+        //     var startTime = DateTime.Now;
+        //     var entities = _client.ExecuteQuery(query).Result;
+        //     var endTime = DateTime.Now;
+        //     var diff = endTime - startTime;
+        //     System.Diagnostics.Trace.WriteLine(diff);
+        //     Assert.AreEqual(entities.Length, NumOfEntries);
+        // }
+        //
+        // [TestCase("sellersqlsql")]
+        // [TestCase("sellermongosql")]
+        // [TestCase("sellerneosql")]
+        // [TestCase("sellerneomongo")]
+        // [TestCase("sellerneoneo")]
+        // [TestCase("sellermongomongo")]
+        // public void TestSellersSellToManyRelations(string entityName)
+        // {
+        //     var query = $"from {entityName} s | select s(name,sellToMany) include(sellToMany(name))";
+        //     var startTime = DateTime.Now;
+        //     var entities = _client.ExecuteQuery(query).Result;
+        //     var endTime = DateTime.Now;
+        //     var diff = endTime - startTime;
+        //     System.Diagnostics.Trace.WriteLine(diff);
+        //     Assert.AreEqual(entities.Length, NumOfEntries);
+        // }
+        //
+        // [TestCase("sellersqlsql")]
+        // [TestCase("sellermongosql")]
+        // [TestCase("sellerneosql")]
+        // [TestCase("sellerneomongo")]
+        // [TestCase("sellerneoneo")]
+        // [TestCase("sellermongomongo")]
+        // public void TestSellersFavoriteBuyerRelations(string entityName)
+        // {
+        //     var query = $"from {entityName} s | select s(name,favoriteBuyer) include(favoriteBuyer(name))";
+        //     var startTime = DateTime.Now;
+        //     var entities = _client.ExecuteQuery(query).Result;
+        //     var endTime = DateTime.Now;
+        //     var diff = endTime - startTime;
+        //     System.Diagnostics.Trace.WriteLine(diff);
+        //     Assert.AreEqual(entities.Length, NumOfEntries);
+        // }
+        //
+        // [TestCase("sellersqlsql")]
+        // [TestCase("sellermongosql")]
+        // [TestCase("sellerneosql")]
+        // [TestCase("sellerneomongo")]
+        // [TestCase("sellerneoneo")]
+        // [TestCase("sellermongomongo")]
+        // public void TestSellersFavoriteBuyerManyRelations(string entityName)
+        // {
+        //     var query = $"from {entityName} s | select s(name,favoriteBuyerMany) include(favoriteBuyerMany(name))";
+        //     var startTime = DateTime.Now;
+        //     var entities = _client.ExecuteQuery(query).Result;
+        //     var endTime = DateTime.Now;
+        //     var diff = endTime - startTime;
+        //     System.Diagnostics.Trace.WriteLine(diff);
+        //     Assert.AreEqual(entities.Length, NumOfEntries);
+        // }
     }
 }
